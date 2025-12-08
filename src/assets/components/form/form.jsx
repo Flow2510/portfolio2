0372f8604805c1b754bfa6a0732cd34e3143f626
Flow2510 <sendra.florian@gmail.com>
2025-12-08 13:27:21 +0100

    ajout de web3form pour le form, ajout d'un modal message envoyé

--- src/assets/components/form/form.jsx
+++ src/assets/components/form/form.jsx
@@ -5,11 +5,14 @@ import { motion } from 'motion/react';
 
 export default function Form() {
   const [name, setName] = useState("");
-  const [nameError, setNameError] = useState(false)
+  const [nameError, setNameError] = useState(false);
   const [email, setEmail] = useState("");
-  const [emailError, setEmailError] = useState(false)
+  const [emailError, setEmailError] = useState(false);
   const [message, setMessage] = useState("");
-  const [messageError, setMessageError] = useState(false)
+  const [messageError, setMessageError] = useState(false);
+  const [messageSend, setMessageSend] = useState(false);
+  const [sendError, setSendError] = useState(false);
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name === "") {
@@ -28,63 +31,99 @@ export default function Form() {
       setMessageError(false);
     }
     if (name !== "" && email !== "" && message !== "") {
-      const subject = encodeURIComponent(`Message de ${name}`);
-      const body = encodeURIComponent(`Nom: ${name}\nEmail: ${email}\n\nMessage:\n${message}`);
-      const mailtoLink = `mailto:sendra.florian@gmail.com?subject=${subject}&body=${body}`;
-      globalThis.location.href = mailtoLink;
+      fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          access_key: "f2e4a252-14c3-43db-a7f6-432bffb121be",
+          name: name,
+          email: email,
+          message: message,
+        }),
+      })
+      .then(async (res) => {
+        const data = await res.json();
+        if (res.status === 200) {
+          console.log("Message envoyé !");
+        } else {
+          console.error("Erreur :", data);
+          setSendError(prev => !prev)
+        }
+        })
+        .catch((error) => {
+          console.error("Erreur réseau :", error);
+        });
+        setMessageSend(prev => !prev);
+        e.target.reset();
+      }
     }
-  }
+
   return (
-    <motion.form 
-      initial={{ opacity: 0, y: 50 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.6, ease: "easeOut" }}
-      viewport={{ once: true, amount: 0.4 }}
-      className="form" onSubmit={handleSubmit}>
-      <div className="form__input-wrapper">
-        <label htmlFor="name" className="sr-only">
-          Nom
-        </label>
-        <input
-          id="name"
-          className={`form__input${nameError ? " form__input--error" : ""}`}
-          type="text"
-          placeholder="Nom"
-          onChange={(e) => setName(e.target.value)}
-        />
-        {nameError && 
-          <p className='form__error form__error-name'>Veuillez insérer un nom</p>
-        }
-        <label htmlFor="email" className="sr-only">
-          Email
-        </label>
-        <input
-          id="email"
-          className={`form__input${emailError ? " form__input--error" : ""}`}
-          type="email"
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        {emailError && 
-          <p className='form__error form__error-name'>Veuillez insérer un email</p>
-        }
-      </div>
-      <div className="form__textarea-wrapper">
-        <label htmlFor="message" className="sr-only">
-          Message
-        </label>
-        <textarea
-          id="message"
-          className={`form__textarea${messageError ? " form__textarea--error" : ""}`}
-          name="message"
-          placeholder="Message"
-          onChange={(e) => setMessage(e.target.value)}
-        ></textarea>
-        {messageError && 
-          <p className='form__error form__error-name'>Veuillez insérer un message</p>
-        }
-      </div>
-      <button className="form__button" type="submit">Envoyer message</button>
-    </motion.form>
+    <>
+      <motion.form 
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, ease: "easeOut" }}
+        viewport={{ once: true, amount: 0.4 }}
+        className="form" onSubmit={handleSubmit}>
+        <div className="form__input-wrapper">
+          <label htmlFor="name" className="sr-only">
+            Nom
+          </label>
+          <input
+            id="name"
+            className={`form__input${nameError ? " form__input--error" : ""}`}
+            type="text"
+            placeholder="Nom"
+            onChange={(e) => setName(e.target.value)}
+          />
+          {nameError && 
+            <p className='form__error form__error-name'>Veuillez insérer un nom</p>
+          }
+          <label htmlFor="email" className="sr-only">
+            Email
+          </label>
+          <input
+            id="email"
+            className={`form__input${emailError ? " form__input--error" : ""}`}
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {emailError && 
+            <p className='form__error form__error-name'>Veuillez insérer un email</p>
+          }
+        </div>
+        <div className="form__textarea-wrapper">
+          <label htmlFor="message" className="sr-only">
+            Message
+          </label>
+          <textarea
+            id="message"
+            className={`form__textarea${messageError ? " form__textarea--error" : ""}`}
+            name="message"
+            placeholder="Message"
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+          {messageError && 
+            <p className='form__error form__error-name'>Veuillez insérer un message</p>
+          }
+        </div>
+        <button className="form__button" type="submit">Envoyer message</button>
+      </motion.form>
+      {messageSend &&
+        <div className='sendmodal'>
+          <div className='sendmodal__wrapper'>
+            <img className='sendmodal__image' src={sendError ? '/images/none.svg' : '/images/check.svg'} alt={sendError? "Checkbox none" : "Checkbox OK"} />
+            <span className='sendmodal__title'>Message envoyé !</span>
+            <p className='sendmodal__text'>{sendError? "Oups ! Votre message n’a pas pu être envoyé. Veuillez réessayer dans un instant." : "Merci ! Votre message a bien été reçu. Je reviendrai vers vous très vite."}</p>
+            <button type='button' className='sendmodal__button' onClick={() => setMessageSend(prev => !prev)}>OK</button>
+          </div>
+        </div>
+      }
+    </>
   );
 }
